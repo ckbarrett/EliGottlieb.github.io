ca25cf6edf0649b41dee6df502843db5f00561d6
Christian Barrett <ckbarret@usc.edu>
2022-05-07 17:32:54 -0700

    Moved classes to their own files

--- gameScripts/sketch.js
+++ gameScripts/sketch.js
@@ -1,67 +1,6 @@
-
 let squareWidth = 30;
 let xOffset = 5; 
 let yOffset = 5;
-
-// Snake Class
-class Snake {
-  constructor() {
-    this.squares = [
-      new Square(0, 0, squareWidth),
-      new Square(xOffset + squareWidth, 0, squareWidth),
-      new Square(xOffset * 2 + squareWidth * 2, 0, squareWidth)
-    ]
-    this.head = this.squares[this.squares.length - 1];
-    this.oldTail = null;
-    this.oldTailxDir = 0;
-    this.oldTailyDir = 0;
-    this.xDir = [1, 1, 1];
-    this.yDir = [0, 0, 0];
-  } 
-  move() {
-    // Update old tail
-    // Make a deep copy
-    let oldTail = this.squares[0];
-    this.oldTail = new Square(oldTail.x, oldTail.y, oldTail.width);
-    // Update all squares in array
-    for(let i = 0; i < this.squares.length; i++){
-      let sq = this.squares[i];
-      sq.x += this.xDir[i] * (xOffset + squareWidth);
-      sq.y += this.yDir[i] * (yOffset + squareWidth);
-    }
-    // Update head
-    this.head = this.squares[this.squares.length - 1];
-    // Update directions
-    this.oldTailxDir = this.xDir[0];
-    this.oldTailyDir = this.yDir[0];
-    for(let i = 0; i < this.xDir.length; i++){
-      if(i == this.xDir.length - 1) { continue; }
-      this.xDir[i] = this.xDir[i + 1];
-      this.yDir[i] = this.yDir[i + 1];
-    }
-  }
-}
-
-// Class Apple
-class Apple {
-  constructor() {
-    this.square = this.getRandomSquare();
-  }
-  getRandomSquare() {
-    let x = 0;
-    let y = 0;
-    do {
-      let numSqauresx = Math.ceil(width / (squareWidth + xOffset));
-      let xCoord = Math.floor(Math.random() * numSqauresx);
-      let numSqauresy = Math.ceil(height / (squareWidth + yOffset));
-      let yCoord = Math.floor(Math.random() * numSqauresy);
-      x = xCoord * (squareWidth + xOffset);
-      y = yCoord * (squareWidth + yOffset);
-    } while(snakeContains(x, y));
-    return new Square(x, y, squareWidth);
-  }
-}
-
 let fr = 15;
 let snake;
 let apple;
@@ -82,11 +21,11 @@ function drawRect(x, y, w, h, clr) {
 }
 
 function drawSnake(){
+  drawSquare(snake.oldTail, color(255,255,255));
+  drawOffset(snake.oldTail, snake.oldTailxDir, snake.oldTailyDir, color(255,255,255));
   drawSquare(snake.head, color(0,255,0));
   drawOffset(snake.squares[snake.squares.length - 2], snake.xDir[snake.xDir.length - 2], 
     snake.yDir[snake.yDir.length - 2], color(0,255,0));
-  drawSquare(snake.oldTail, color(255,255,255));
-  drawOffset(snake.oldTail, snake.oldTailxDir, snake.oldTailyDir, color(255,255,255));
 }
 
 function drawSnakeComplete() {
---
+++ gameScripts/snake.js
@@ -0,0 +1,39 @@
+// Snake Class
+class Snake {
+    constructor() {
+      this.squares = [
+        new Square(0, 0, squareWidth),
+        new Square(xOffset + squareWidth, 0, squareWidth),
+        new Square(xOffset * 2 + squareWidth * 2, 0, squareWidth)
+      ]
+      this.head = this.squares[this.squares.length - 1];
+      this.oldTail = null;
+      this.oldTailxDir = 0;
+      this.oldTailyDir = 0;
+      this.xDir = [1, 1, 1];
+      this.yDir = [0, 0, 0];
+    } 
+    move() {
+      // Update old tail
+      // Make a deep copy
+      let oldTail = this.squares[0];
+      this.oldTail = new Square(oldTail.x, oldTail.y, oldTail.width);
+      // Update all squares in array
+      for(let i = 0; i < this.squares.length; i++){
+        let sq = this.squares[i];
+        sq.x += this.xDir[i] * (xOffset + squareWidth);
+        sq.y += this.yDir[i] * (yOffset + squareWidth);
+      }
+      // Update head
+      this.head = this.squares[this.squares.length - 1];
+      // Update directions
+      this.oldTailxDir = this.xDir[0];
+      this.oldTailyDir = this.yDir[0];
+      for(let i = 0; i < this.xDir.length; i++){
+        if(i == this.xDir.length - 1) { continue; }
+        this.xDir[i] = this.xDir[i + 1];
+        this.yDir[i] = this.yDir[i + 1];
+      }
+    }
+  }
+  
